451da955ec5ecf110dea10529e49e606b5833fa3
Ariel Santos <ariel.santos@dcc.ufmg.br>
2020-07-18 16:06:14 -0300

    Melhorando manipulação do name em inputs mistos

--- app/javascript/controllers/composite_field_controller.js
+++ app/javascript/controllers/composite_field_controller.js
@@ -4,11 +4,11 @@ export default class extends Controller {
     static targets = ['input'];
 
     connect() {
-        if(!this.data.has('fieldName'))
+        if (!this.data.has('fieldName'))
             this.data.set('fieldName', this.inputTargets[0].name)
 
-        this.inputTargets.forEach( input => {
-            if(input.checked === true || input.selected === true)
+        this.inputTargets.forEach(input => {
+            if (input.checked === true || input.selected === true)
                 this.clear_others(input)
             input.addEventListener('input', ev => {
                 this.clear_others(ev.target)
@@ -19,10 +19,10 @@ export default class extends Controller {
     clear_others = (modified_input) => {
         modified_input.name = this.data.get('fieldName')
 
-        this.inputTargets.forEach( input => {
+        this.inputTargets.forEach(input => {
             if (input === modified_input) return
 
-            input.name = null
+            input.removeAttribute('name')
             if (input.type === 'radio')
                 input.checked = false
             else if (input.type === 'checkbox')
