bdcf0be82fd1e96578b8f4063d2423d0a6d27906
Ariel Santos <ariel.santos@dcc.ufmg.br>
2020-07-21 18:41:53 -0300

    Adicionando monitoramento de erros pelo Sentry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_raven_context
+
   protected
 
   def after_sign_in_path_for(user)
@@ -9,5 +11,12 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(_)
     new_user_session_path
+  end
+
+  private
+
+  def set_raven_context
+    Raven.user_context(id: request.user_agent) # or anything else in session
+    Raven.extra_context(user: current_user&.id)
   end
 end
